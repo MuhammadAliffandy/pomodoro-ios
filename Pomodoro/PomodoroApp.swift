362586d4233dfd72993b984bd70b889955b3c3a1
Brian Anashari <brianashari18@gmail.com>
2026-03-03 14:09:37 +0700

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by academy on 03/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
